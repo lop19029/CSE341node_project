ccec4517998e602e1c18cb57b68b2c7a7128ac10
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-05 21:19:23 -0500

    Working validations!

--- controllers/public.js
+++ controllers/public.js
@@ -19,12 +19,12 @@ const nodemailer = require('nodemailer');
 
 // Transporter
 const transporter = nodemailer.createTransport({
-    host: "mail.domain",
+    host: "mail.sacredplanner.xyz",
     port: 465,
     secure: true,
     auth: {
-        user: "email@domain", // generated ethereal user
-        pass: "password" // generated ethereal password
+        user: "nodemailer@sacredplanner.xyz", // generated ethereal user
+        pass: "mTtBAXRdEf" // generated ethereal password
     }
 });
 
@@ -66,6 +66,76 @@ exports.getLogin = (req, res, next) => {
         PagetoLoad: 'auth/login',
         SocialLinks: socialLinks
     });
+};
+
+//Signup
+exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    res.render('template', {
+        pageTitle: 'Signup',
+        PagetoLoad: 'auth/signup',
+        SocialLinks: socialLinks,
+        errorMessage: message,
+        oldInput: {
+            name: '',
+            mail: '',
+            password: ''
+        },
+        validationErrors: []
+    });
+};
+
+//Signup
+exports.postSignup = (req, res, next) => {
+    const {
+        uName,
+        mail,
+        password
+    } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render('template', {
+            pageTitle: 'Signup',
+            PagetoLoad: 'auth/signup',
+            SocialLinks: socialLinks,
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                uName: uName,
+                mail: mail,
+                password: password
+            },
+            validationErrors: errors.array()
+        });
+    }
+    transporter.sendMail({
+            to: 'marcoantonio@marbust.com', //Please add your personal email where you'll receive the contact form response
+            from: mail,
+            subject: 'Formulario de Contactos | ' + uName,
+            html: `
+        <h1 style='text-align: center;'>Formulario de Contacto</h1>
+        <hr>
+    <ul style='line-height: 2em;'>
+    <li><strong>Nombre:</strong> ${uName}</li>
+    <li><strong>Correo:</strong> <a href="mailto:${mail}">${mail}</a></li>
+    </ul>
+    <hr>
+    <p style='text-align: center;'><strong>Form made with Marbust Websites&reg;'s Technology under the Marbust Technology Company License</strong></p>
+    `
+        }).then(function (success) {
+            req.flash('error', 'Mensaje Enviado Correctamente!');
+            res.redirect('/signup');
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 //Contact
@@ -117,7 +187,7 @@ exports.postContact = (req, res, next) => {
         });
     }
     transporter.sendMail({
-            to: 'xxxxxxxx@xxxxxx.xxxxx', //Please add your personal email where you'll receive the contact form response
+            to: 'marcoantonio@marbust.com', //Please add your personal email where you'll receive the contact form response
             from: mail,
             subject: 'Formulario de Contactos | ' + name,
             html: `
--- routes/public.js
+++ routes/public.js
@@ -17,6 +17,28 @@ router.get('/about', publicController.getAbout);
 router.get('/services', publicController.getServices);
 //Services
 router.get('/login', publicController.getLogin);
+
+router.get('/signup', publicController.getSignup);
+
+
+router.post('/signup',
+[
+body('uName', 'Please insert your name in the right way.')
+.isString()
+.isLength({
+    min: 3
+})
+.trim(),
+body('mail', 'Por favor ingrese su email correctamente.')
+.isEmail()
+.normalizeEmail(),
+body('password', 'Por favor ingrese su contraseña correctamente.')
+.isString()
+.isLength({
+    min: 5
+})
+],
+publicController.postSignup);
 //Contact
 router.get('/contact', publicController.getContact);
 //Post Contact
