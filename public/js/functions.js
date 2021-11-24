452e0d67d0850c6bbec4c60feb7511e24257d4e1
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-23 21:00:36 -0500

    Fix!

--- public/js/functions.js
+++ public/js/functions.js
@@ -40,23 +40,7 @@ document.getElementById("close-btn").addEventListener("click", () => {
 });
 
 
-document.getElementById("nMeetingBtn").addEventListener("click", () => {
-    document.getElementById("isNormalMeeting").style.display = "block";
-    document.getElementById("finalAgendaOptions").style.display = "block";
-    document.getElementById("meetingKind").value = "isNormal";
-    document.getElementById("btn-agenda").disabled = false;
-    document.getElementById("nMeetingBtn").classList.add("btn-active");
-    document.getElementById("fMeetingBtn").classList.remove("btn-active");
-});
 
-document.getElementById("fMeetingBtn").addEventListener("click", () => {
-    document.getElementById("isNormalMeeting").style.display = "none";
-    document.getElementById("finalAgendaOptions").style.display = "block";
-    document.getElementById("meetingKind").value = "isFast";
-    document.getElementById("btn-agenda").disabled = false;
-    document.getElementById("nMeetingBtn").classList.remove("btn-active");
-    document.getElementById("fMeetingBtn").classList.add("btn-active");
-});
 
 // document.getElementById("pop-up").addEventListener("click", () => {
 //     var popup = document.getElementById("pop-up");
